7a81351cd78faf64f3d019c0d2800406d7d2a94e
Dario Cangialosi <dario.cangialosi@gmail.com>
2017-06-02 17:54:39 +0200

    working

---
+++ wbencoder_wav2amr.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Usage: encoder.sh wav_input_file amr_output_file (./wbencoder_wav2amr.sh wave_se_t630.wav amrwb_se_t360.amr)
+
+#sox -t raw -r 8000 -e signed-integer -b 16 --endian little -c 1 se_t630.raw wave_se_t630.wav
+
+sox $1 -t raw -r 16000 -e signed-integer -b 16 --endian little -c 1 $1.sw upsample > /dev/null 2>&1
+
+./amrwb-encoder 8 $1.sw $2 > /dev/null 2>&1
+
+rm $1 > /dev/null 2>&1
+
+play $2 > /dev/null 2>&1
+
---
+++ wbencoding_example.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# sox's 'play' command (plays audio)
+#play -t raw -r 8000 -e signed-integer -b 16 --endian little -c 1 se_t630.raw
+#play -t raw -r 16000 -e signed-integer -b 16 --endian little -c 1 wb_out.sw
+
+# sox's 'sox' command (conversion tool) 
+## sox -t raw -r 8000 -e signed-integer -b 16 --endian little -c 1 se_t630.raw out.wav
+
+echo "CONVERT raw to .sw (.sw is a sox shorthand for signed word .raw)"
+sox -t raw -r 8000 -e signed-integer -b 16 --endian little -c 1 se_t630.raw -r 16000 wb_out.sw upsample > /dev/null 2>&1
+
+echo "PLAY .sw raw audio file"
+play -t raw -r 16000 -e signed-integer -b 16 --endian little -c 1 wb_out.sw > /dev/null 2>&1
+
+echo "ENCODE signed word raw(.sw) to wide-band(wb) AMR ..."
+./amrwb-encoder 8 wb_out.sw wb_out.amr > /dev/null 2>&1
+echo "... and PLAY it with sox's play command"
+play wb_out.amr > /dev/null 2>&1
+
+# open AMR with VLC
+## vlc --intf ncurses se_t630.amr
+#vlc --intf ncurses wb_out.amr
